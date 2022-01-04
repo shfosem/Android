adc61cc45f2759247d7dbbbe502b31daa8ad2bea
shfosem <ssh9806@naver.com>
2022-01-04 20:09:35 +0900

    Chapter5_2 example

---
+++ Chapter5_2/SampleListView2/app/src/main/java/org/androidtown/samplelistview2/MainActivity.java
@@ -0,0 +1,32 @@
+package org.androidtown.samplelistview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    String[] items = {"mike", "angel", "crow","john","ginnie", "sally", "cohen", "rice"};
+
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+
+        setListAdapter(new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,items
+        ));
+    }
+    protected void onListItemClick(ListView list, View v, int position, long id){
+       super.onListItemClick(list,v,position,id);
+       String text = "position : "+ position + " " + items[position];
+        Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
+    }
+}
